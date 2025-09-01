8a589f3295cc4ea5d17a01ea6a4382e20d120779
Yesid Castro <castrogil202@gmail.com>
2025-09-01 17:29:12 -0500

    feat: register.js

--- APP/frontend/src/js/register.js
+++ APP/frontend/src/js/register.js
@@ -329,7 +329,6 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-<<<<<<< HEAD
     // Enlaces a login
     const loginLinks = document.querySelectorAll('.login-link a');
     loginLinks.forEach(link => {
@@ -339,9 +338,3 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
-=======
-    console.log('✅ Event listeners successfully registered');
-    console.log('🔗 Google OAuth URL: http://localhost:3000/auth/google');
-});
-
->>>>>>> 011c7002e37d12110a02b034f21550b76425ace6

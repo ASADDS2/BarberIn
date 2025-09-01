00fc408598a00a6103922b04c7d728cc1d10c28a
wilffren <wilffren14@gmail.com>
2025-09-01 12:10:21 -0500

    Feat: Route to fronted multiple ports

--- APP/backend/app.js
+++ APP/backend/app.js
@@ -13,9 +13,17 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:3001'];
+
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3001',
-    credentials: true
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
 }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
